58b5d03ef4d5f366132b7de39e9cb1a82989d7d9
Ed Summers <ehs@pobox.com>
2013-04-22 05:41:03 -0400

    use uri instead of url, since we might be storing non http urls

--- annotator/annotation.py
+++ annotator/annotation.py
@@ -108,6 +108,11 @@ class Annotation(es.Model):
     __settings__ = SETTINGS
 
     @classmethod
+    def locate(cls, uri, offset=0, limit=20):
+        q = cls._builds_query(offset, limit)
+        print q
+
+    @classmethod
     def _build_query(cls, offset=0, limit=20, **kwargs):
         q = super(Annotation, cls)._build_query(offset, limit, **kwargs)
 
